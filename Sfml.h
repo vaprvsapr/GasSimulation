1be834e9947a21c1d0bad6460b95628431389056
vaprvsapr <vaprvsapr@mail.ru>
2021-11-24 22:25:58 +0300

    Implemented class SimulationWindow. Now it is easier to create a simulation window;

---
+++ Sfml.h
@@ -0,0 +1,61 @@
+//
+// Created by nikita on 24.11.2021.
+//
+
+#ifndef MAIN_CPP_SFML_H
+#define MAIN_CPP_SFML_H
+
+#include "SFML/Graphics.hpp"
+#include "System.h"
+
+class SimulationWindow {
+private:
+    sf::RenderWindow window;
+    sf::RectangleShape black_background;
+    System& system;
+
+public:
+    explicit SimulationWindow(System &_system) :
+    system(_system),
+    window(sf::RenderWindow(sf::VideoMode(
+            unsigned(_system.GetSystemSize().x),
+            unsigned(_system.GetSystemSize().y)),
+                            "Simulation window.")),
+    black_background({float(_system.GetSystemSize().x),
+                      float(_system.GetSystemSize().y)})
+    {
+        black_background.setFillColor((sf::Color::Black));
+    }
+
+    void ShowSimulationWindow()
+    {
+        while(window.isOpen())
+        {
+            window.clear();
+            sf::Event event{};
+            while(window.pollEvent(event))
+            {
+                if(event.type == sf::Event::Closed)
+                    window.close();
+            }
+
+            for(auto& particle : system.GetParticles())
+            {
+                sf::CircleShape circle_shape;
+                circle_shape.setRadius(float(particle.properties.radius));
+                circle_shape.setPosition(
+                        float(particle.position.x - particle.properties.radius),
+                        float(particle.position.y - particle.properties.radius));
+                window.draw(circle_shape);
+            }
+
+            system.OperatorMove();
+            system.OperatorCollideWithBorder();
+            system.OperatorCollideWithParticle(MODE::SECOND);
+            window.display();
+        }
+        }
+
+};
+
+#endif //MAIN_CPP_SFML_H
--- main.cpp
+++ main.cpp
@@ -1,48 +1,53 @@
 #include "SFML/Graphics.hpp"
 #include <iostream>
 #include "System.h"
+#include "Sfml.h"
 
 int main()
 {
     System system({1000, 800});
+    SimulationWindow simulation_window(system);
 
 
-    system.AddParticles(10000, 1, {1, 1});
+    system.AddParticles(1000, 1, {1, 1});
+
+    simulation_window.ShowSimulationWindow();
 //    system.AddParticles(1, 1, {20, 40});
 //    system.AddParticle({30, 101}, {1, 0}, {10, 2});
 //    system.AddParticle({200, 100}, {0, 0}, {10, 1});
 
-    sf::RenderWindow window(sf::VideoMode(unsigned(system.GetSystemSize().x), unsigned(system.GetSystemSize().y)), "SFML works!");
-    sf::RectangleShape BlackBackground({float(system.GetSystemSize().x), float(system.GetSystemSize().y)});
-    BlackBackground.setFillColor((sf::Color::Black));
 
-    while(window.isOpen()) {
-        window.clear();
-        sf::Event event{};
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        vector<sf::CircleShape> shapes;
-        for(auto& particle : system.GetParticles())
-        {
-            sf::CircleShape shape;
-            shape.setRadius(float(particle.properties.radius));
-            shape.setPosition(float(particle.position.x - particle.properties.radius), float(particle.position.y - particle.properties.radius));
-            window.draw(shape);
-        }
-
-        system.OperatorMove();
-        system.OperatorCollideWithBorder();
-        system.OperatorCollideWithParticle(MODE::SECOND);
-//        system.OperatorCollideWithParticleComplexityNSquaredDividedByM(20);
-//        system.OperatorCollideWithParticleComplexityNSquared();
-//        system.OperatorGravity();
-//        cout << "energy: " << system.OperatorComputeEnergy() << endl;
-        cout << "average number of collisions: " << system.AverageNumberOfCollisions() << endl;
-
-        window.display();
-    }
+//    sf::RenderWindow window(sf::VideoMode(unsigned(system.GetSystemSize().x), unsigned(system.GetSystemSize().y)), "SFML works!");
+//    sf::RectangleShape BlackBackground({float(system.GetSystemSize().x), float(system.GetSystemSize().y)});
+//    BlackBackground.setFillColor((sf::Color::Black));
+//
+//    while(window.isOpen()) {
+//        window.clear();
+//        sf::Event event{};
+//        while (window.pollEvent(event)) {
+//            if (event.type == sf::Event::Closed)
+//                window.close();
+//        }
+//
+//        vector<sf::CircleShape> shapes;
+//        for(auto& particle : system.GetParticles())
+//        {
+//            sf::CircleShape shape;
+//            shape.setRadius(float(particle.properties.radius));
+//            shape.setPosition(float(particle.position.x - particle.properties.radius), float(particle.position.y - particle.properties.radius));
+//            window.draw(shape);
+//        }
+//
+//        system.OperatorMove();
+//        system.OperatorCollideWithBorder();
+//        system.OperatorCollideWithParticle(MODE::SECOND);
+////        system.OperatorCollideWithParticleComplexityNSquaredDividedByM(20);
+////        system.OperatorCollideWithParticleComplexityNSquared();
+////        system.OperatorGravity();
+////        cout << "energy: " << system.OperatorComputeEnergy() << endl;
+//        cout << "average number of collisions: " << system.AverageNumberOfCollisions() << endl;
+//
+//        window.display();
+//    }
 
 }
