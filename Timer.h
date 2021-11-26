8d3c2046b2729ed118c062972098d2d905461dba
vaprvsapr <vaprvsapr@mail.ru>
2021-11-26 14:37:51 +0300

    Implemented class Timer. To measure time.

---
+++ Timer.h
@@ -0,0 +1,30 @@
+//
+// Created by nikita on 26.11.2021.
+//
+
+#ifndef MAIN_CPP_TIMER_H
+#define MAIN_CPP_TIMER_H
+
+#include <ctime>
+#include <string>
+#include <iostream>
+
+class Timer
+{
+private:
+clock_t time;
+std::string message;
+public:
+    Timer(std::string _message)
+    {
+        message = _message;
+        time = clock();
+    }
+    ~Timer()
+    {
+        std::cout << clock() - time;
+    }
+};
+
+
+#endif //MAIN_CPP_TIMER_H
